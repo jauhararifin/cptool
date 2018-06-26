430143794320d1dd06eee0770726afc20623c8dd
Jauhar Arifin <jauhararifin10@gmail.com>
2018-06-26 10:28:10 +0700

    add install script

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # CPTOOL
 
 Simple bash script to compile and run your solution when solving competitive programming problems.
+
+## How To Install
+
+Run this command in your bash shell
+
+```
+curl -L -s https://raw.githubusercontent.com/jauhararifin/cptool/master/install.sh | bash
+```
---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+INSTALL_DIR=$HOME/.cptool
+
+curl -Lo /tmp/cptool.tar.gz https://github.com/jauhararifin/cptool/archive/v0.0.0.tar.gz > /dev/null 2> /dev/null
+mkdir -p $INSTALL_DIR
+tar -xf /tmp/cptool.tar.gz -C $INSTALL_DIR --strip 1
+
+if [ -z $CPTOOL_DIR ]; then
+    export CPTOOL_DIR="$INSTALL_DIR"
+    export PATH=$PATH:$CPTOOL_DIR
+    echo "" >> $HOME/.bashrc
+    echo "export CPTOOL_DIR=\"$INSTALL_DIR\"" >> $HOME/.bashrc
+    echo "export PATH=\"\$PATH:\$CPTOOL_DIR\"" >> $HOME/.bashrc
+fi
+
+echo "Installed"
