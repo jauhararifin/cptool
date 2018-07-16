699aa684612959904af05ae084eb77cc2be912cc
Jauhar Arifin <jauhararifin10@gmail.com>
2018-07-16 07:29:36 +0700

    fix executioner utility

--- internal/executioner/memexec.go
+++ internal/executioner/memexec.go
@@ -23,18 +23,18 @@ type MemCmd struct {
 
 // NewMemExec not yet defined
 func NewMemExec() Exec {
-	return MemExec{}
+	return &MemExec{}
 }
 
 // Command returns the Cmd struct to execute the named program with the given arguments
-func (MemExec) Command(name string, arg ...string) Cmd {
-	return &MemCmd{}
+func (m *MemExec) Command(name string, arg ...string) Cmd {
+	return &MemCmd{MemExec: *m}
 }
 
 // CombinedOutput implements CombinedOutput of Cmd
 func (m *MemCmd) CombinedOutput() ([]byte, error) {
 	if m.CombinedOutputCallback != nil {
-		m.CombinedOutputCallback()
+		return m.CombinedOutputCallback()
 	}
 	return make([]byte, 0), nil
 }
@@ -42,7 +42,7 @@ func (m *MemCmd) CombinedOutput() ([]byte, error) {
 // Output implements Output of Cmd
 func (m *MemCmd) Output() ([]byte, error) {
 	if m.OutputCallback != nil {
-		m.OutputCallback()
+		return m.OutputCallback()
 	}
 	return make([]byte, 0), nil
 }
@@ -50,7 +50,7 @@ func (m *MemCmd) Output() ([]byte, error) {
 // Run implements Run of Cmd
 func (m *MemCmd) Run() error {
 	if m.RunCallback != nil {
-		m.RunCallback()
+		return m.RunCallback()
 	}
 	return nil
 }
@@ -58,7 +58,7 @@ func (m *MemCmd) Run() error {
 // Start implements Start of Cmd
 func (m *MemCmd) Start() error {
 	if m.StartCallback != nil {
-		m.StartCallback()
+		return m.StartCallback()
 	}
 	return nil
 }
@@ -66,7 +66,7 @@ func (m *MemCmd) Start() error {
 // StderrPipe implements StderrPipe of Cmd
 func (m *MemCmd) StderrPipe() (io.ReadCloser, error) {
 	if m.StderrPipeCallback != nil {
-		m.StderrPipeCallback()
+		return m.StderrPipeCallback()
 	}
 	return nil, nil
 }
@@ -74,7 +74,7 @@ func (m *MemCmd) StderrPipe() (io.ReadCloser, error) {
 // StdinPipe implements StdinPipe of Cmd
 func (m *MemCmd) StdinPipe() (io.WriteCloser, error) {
 	if m.StdinPipeCallback != nil {
-		m.StdinPipeCallback()
+		return m.StdinPipeCallback()
 	}
 	return nil, nil
 }
@@ -82,7 +82,7 @@ func (m *MemCmd) StdinPipe() (io.WriteCloser, error) {
 // StdoutPipe implements StdoutPipe of Cmd
 func (m *MemCmd) StdoutPipe() (io.ReadCloser, error) {
 	if m.StdoutPipeCallback != nil {
-		m.StdoutPipeCallback()
+		return m.StdoutPipeCallback()
 	}
 	return nil, nil
 }
@@ -90,7 +90,7 @@ func (m *MemCmd) StdoutPipe() (io.ReadCloser, error) {
 // Wait implements Wait of Cmd
 func (m *MemCmd) Wait() error {
 	if m.WaitCallback != nil {
-		m.WaitCallback()
+		return m.WaitCallback()
 	}
 	return nil
 }
--- internal/executioner/osexec.go
+++ internal/executioner/osexec.go
@@ -2,14 +2,15 @@ package executioner
 
 import osexec "os/exec"
 
+// OsExec implemens exec from os package
 type OsExec struct{}
 
-// NewOSExec not yet defined
+// NewOSExec create new OsExec instance
 func NewOSExec() Exec {
-	return OsExec{}
+	return &OsExec{}
 }
 
 // Command returns the Cmd struct to execute the named program with the given arguments
-func (OsExec) Command(name string, arg ...string) Cmd {
+func (*OsExec) Command(name string, arg ...string) Cmd {
 	return osexec.Command(name, arg...)
 }
