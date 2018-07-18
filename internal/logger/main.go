3ead8e6964b54caa775fd6e4c19e488d5a350d04
Jauhar Arifin <jauhararifin10@gmail.com>
2018-07-18 07:02:59 +0700

    add color for logger

--- internal/logger/main.go
+++ internal/logger/main.go
@@ -4,28 +4,28 @@ import "fmt"
 
 // PrintInfo print message in screen with INFO level
 func PrintInfo(messages ...interface{}) {
-	fmt.Print("[   Info    ] ")
+	fmt.Printf("\033[0;34m[   Info    ] ")
 	fmt.Print(messages...)
-	fmt.Println()
+	fmt.Printf("\033[0m\n")
 }
 
 // PrintWarning print message in screen with WARNING level
 func PrintWarning(messages ...interface{}) {
-	fmt.Print("[   Warn    ] ")
+	fmt.Print("\033[0;93m[   Warn    ] ")
 	fmt.Print(messages...)
-	fmt.Println()
+	fmt.Printf("\033[0m\n")
 }
 
 // PrintError print message in screen with ERROR level
 func PrintError(messages ...interface{}) {
-	fmt.Print("[   Error   ] ")
+	fmt.Print("\033[0;31m[   Error   ] ")
 	fmt.Print(messages...)
-	fmt.Println()
+	fmt.Printf("\033[0m\n")
 }
 
 // PrintSuccess print message in screen with SUCCESS level
 func PrintSuccess(messages ...interface{}) {
-	fmt.Print("[  Success  ] ")
+	fmt.Print("\033[0;32m[  Success  ] ")
 	fmt.Print(messages...)
-	fmt.Println()
+	fmt.Printf("\033[0m\n")
 }
