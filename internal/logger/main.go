1fef858ccdb043bf81f2098a2373e3b160bf9e45
Jauhar Arifin <jauhararifin10@gmail.com>
2018-07-20 05:52:13 +0700

    using stderr for log

--- internal/logger/main.go
+++ internal/logger/main.go
@@ -1,31 +1,34 @@
 package logger
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 // PrintInfo print message in screen with INFO level
 func PrintInfo(messages ...interface{}) {
-	fmt.Printf("\033[0;34m[   Info    ] ")
-	fmt.Print(messages...)
-	fmt.Printf("\033[0m\n")
+	fmt.Fprintf(os.Stderr, "\033[0;34m[   Info    ] ")
+	fmt.Fprint(os.Stderr, messages...)
+	fmt.Fprintf(os.Stderr, "\033[0m\n")
 }
 
 // PrintWarning print message in screen with WARNING level
 func PrintWarning(messages ...interface{}) {
-	fmt.Print("\033[0;93m[   Warn    ] ")
-	fmt.Print(messages...)
-	fmt.Printf("\033[0m\n")
+	fmt.Fprint(os.Stderr, "\033[0;93m[   Warn    ] ")
+	fmt.Fprint(os.Stderr, messages...)
+	fmt.Fprintf(os.Stderr, "\033[0m\n")
 }
 
 // PrintError print message in screen with ERROR level
 func PrintError(messages ...interface{}) {
-	fmt.Print("\033[0;31m[   Error   ] ")
-	fmt.Print(messages...)
-	fmt.Printf("\033[0m\n")
+	fmt.Fprint(os.Stderr, "\033[0;31m[   Error   ] ")
+	fmt.Fprint(os.Stderr, messages...)
+	fmt.Fprintf(os.Stderr, "\033[0m\n")
 }
 
 // PrintSuccess print message in screen with SUCCESS level
 func PrintSuccess(messages ...interface{}) {
-	fmt.Print("\033[0;32m[  Success  ] ")
-	fmt.Print(messages...)
-	fmt.Printf("\033[0m\n")
+	fmt.Fprint(os.Stderr, "\033[0;32m[  Success  ] ")
+	fmt.Fprint(os.Stderr, messages...)
+	fmt.Fprintf(os.Stderr, "\033[0m\n")
 }
