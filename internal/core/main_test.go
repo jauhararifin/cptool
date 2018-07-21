4055b2b89eb9841a49e0a4c3ebfe8569eb8485bf
Jauhar Arifin <jauhararifin10@gmail.com>
2018-07-21 07:14:48 +0700

    add test for main cptool

--- internal/core/main_test.go
+++ internal/core/main_test.go
@@ -1,6 +1,10 @@
 package core
 
 import (
+	"os"
+	"os/user"
+	"testing"
+
 	"github.com/jauhararifin/cptool/internal/executioner"
 	"github.com/spf13/afero"
 )
@@ -24,4 +28,40 @@ func newTest() *CPTool {
 func getCptoolMemExec(cptool *CPTool) *executioner.MemExec {
 	memExec, _ := cptool.exec.(*executioner.MemExec)
 	return memExec
+}
+
+func TestNewDefault(t *testing.T) {
+	cptool, err := NewDefault()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if cptool.MajorVersion != MajorVersion {
+		t.Error("NewDefault should returns cptool with MajorVersion=" + string(MajorVersion))
+	}
+	if cptool.MinorVersion != MinorVersion {
+		t.Error("NewDefault should returns cptool with MinorVersion=" + string(MinorVersion))
+	}
+	if cptool.PatchVersion != PatchVersion {
+		t.Error("NewDefault should returns cptool with PatchVersion=" + string(PatchVersion))
+	}
+
+	currentDirectory, _ := os.Getwd()
+	if cptool.workingDirectory != currentDirectory {
+		t.Error("NewDefault should returns cptool with workingDirectory points to current working directory")
+	}
+
+	currentUser, _ := user.Current()
+	currentHome := currentUser.HomeDir
+	if cptool.homeDirectory != currentHome {
+		t.Error("NewDefault should returns cptool with homeDirectory points to current home directory")
+	}
+}
+
+func TestBootstrap(t *testing.T) {
+	cptool, _ := NewDefault()
+	err := cptool.Bootstrap()
+	if err != nil {
+		t.Error(err)
+	}
 }
