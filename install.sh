69c33b39d5ca759375f12fd7a2499ecf49fa9ece
Jauhar Arifin <jauhararifin10@gmail.com>
2018-07-22 12:00:08 +0700

    remove --strip 1

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ CPTOOL_NAME=cptool_${VERSION}_linux_amd64.tar.gz
 
 curl -Lo /tmp/cptool.tar.gz https://github.com/jauhararifin/cptool/releases/download/v$VERSION/$CPTOOL_NAME > /dev/null 2> /dev/null
 mkdir -p $INSTALL_DIR
-tar -xf /tmp/cptool.tar.gz -C $INSTALL_DIR --strip 1
+tar -xf /tmp/cptool.tar.gz -C $INSTALL_DIR
 
 if [ -z $CPTOOL_DIR ]; then
     export CPTOOL_DIR="$INSTALL_DIR"
