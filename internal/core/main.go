06bd7607d633b394c7578bb231a1c7dde341935a
Jauhar Arifin <jauhararifin10@gmail.com>
2018-07-22 12:06:22 +0700

    fix language

--- internal/core/main.go
+++ internal/core/main.go
@@ -9,10 +9,10 @@ import (
 )
 
 // MajorVersion incidates current cptool major version
-const MajorVersion = 0
+const MajorVersion = 1
 
 // MinorVersion incidates current cptool minor version
-const MinorVersion = 1
+const MinorVersion = 0
 
 // PatchVersion incidates current cptool patch version
 const PatchVersion = 1
--- internal/core/version.go
+++ internal/core/version.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-// GetVersion returns current version as string. Example: "v.0.1.0"
+// GetVersion returns current version as string. Example: "v.1.0.1"
 func (cptool *CPTool) GetVersion() string {
 	return fmt.Sprintf("v%d.%d.%d", cptool.MajorVersion, cptool.MinorVersion, cptool.PatchVersion)
 }
