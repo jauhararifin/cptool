270bbef9a1bfa6f0646e435746b1f0b14ad6f054
Jauhar Arifin <jauhararifin10@gmail.com>
2018-07-22 11:43:27 +0700

    fix install.sh link

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ INSTALL_DIR=$HOME/.cptool
 VERSION=1.0.0
 CPTOOL_NAME=cptool_${VERSION}_linux_amd64.tar.gz
 
-curl -Lo /tmp/cptool.tar.gz https://github.com/jauhararifin/releases/download/v$VERSION/$CPTOOL_NAME > /dev/null 2> /dev/null
+curl -Lo /tmp/cptool.tar.gz https://github.com/jauhararifin/cptool/releases/download/v$VERSION/$CPTOOL_NAME > /dev/null 2> /dev/null
 mkdir -p $INSTALL_DIR
 tar -xf /tmp/cptool.tar.gz -C $INSTALL_DIR --strip 1
 
