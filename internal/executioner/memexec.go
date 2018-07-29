55e908f5dd86bd53d43c2a7db56f4a0bddceeaaa
Jauhar Arifin <jauhararifin10@gmail.com>
2018-07-29 16:35:57 +0700

    update memexec

--- internal/executioner/memexec.go
+++ internal/executioner/memexec.go
@@ -8,45 +8,46 @@ import (
 
 // MemExec implements Exec using callbacks
 type MemExec struct {
-	CombinedOutputCallback func(*BaseCmd) ([]byte, error)
-	OutputCallback         func(*BaseCmd) ([]byte, error)
-	RunCallback            func(*BaseCmd) error
-	StartCallback          func(*BaseCmd) error
-	StderrPipeCallback     func(*BaseCmd) (io.ReadCloser, error)
-	StdinPipeCallback      func(*BaseCmd) (io.WriteCloser, error)
-	StdoutPipeCallback     func(*BaseCmd) (io.ReadCloser, error)
-	WaitCallback           func(*BaseCmd) error
+	CombinedOutputCallback func(*MemCmd) ([]byte, error)
+	OutputCallback         func(*MemCmd) ([]byte, error)
+	RunCallback            func(*MemCmd) error
+	StartCallback          func(*MemCmd) error
+	StderrPipeCallback     func(*MemCmd) (io.ReadCloser, error)
+	StdinPipeCallback      func(*MemCmd) (io.WriteCloser, error)
+	StdoutPipeCallback     func(*MemCmd) (io.ReadCloser, error)
+	WaitCallback           func(*MemCmd) error
 }
 
 // MemCmd implements Cmd using memory
 type MemCmd struct {
 	*BaseCmd
 	MemExec
+	Context context.Context
 }
 
 // NewMemExec not yet defined
-func NewMemExec() Exec {
+func NewMemExec() *MemExec {
 	return &MemExec{}
 }
 
 // Command returns the Cmd struct to execute the named program with the given arguments
 func (m *MemExec) Command(name string, arg ...string) Cmd {
 	oscmd := exec.Command(name, arg...)
-	basecmd := &BaseCmd{oscmd}
-	return &MemCmd{MemExec: *m, BaseCmd: basecmd}
+	basecmd := &BaseCmd{Cmd: oscmd}
+	return &MemCmd{MemExec: *m, BaseCmd: basecmd, Context: nil}
 }
 
 // CommandContext create command with context
 func (m *MemExec) CommandContext(ctx context.Context, name string, arg ...string) Cmd {
 	oscmd := exec.CommandContext(ctx, name, arg...)
-	basecmd := &BaseCmd{oscmd}
-	return &MemCmd{MemExec: *m, BaseCmd: basecmd}
+	basecmd := &BaseCmd{Cmd: oscmd}
+	return &MemCmd{MemExec: *m, BaseCmd: basecmd, Context: ctx}
 }
 
 // CombinedOutput implements CombinedOutput of Cmd
 func (m *MemCmd) CombinedOutput() ([]byte, error) {
 	if m.CombinedOutputCallback != nil {
-		return m.CombinedOutputCallback(m.BaseCmd)
+		return m.CombinedOutputCallback(m)
 	}
 	return make([]byte, 0), nil
 }
@@ -54,7 +55,7 @@ func (m *MemCmd) CombinedOutput() ([]byte, error) {
 // Output implements Output of Cmd
 func (m *MemCmd) Output() ([]byte, error) {
 	if m.OutputCallback != nil {
-		return m.OutputCallback(m.BaseCmd)
+		return m.OutputCallback(m)
 	}
 	return make([]byte, 0), nil
 }
@@ -62,7 +63,7 @@ func (m *MemCmd) Output() ([]byte, error) {
 // Run implements Run of Cmd
 func (m *MemCmd) Run() error {
 	if m.RunCallback != nil {
-		return m.RunCallback(m.BaseCmd)
+		return m.RunCallback(m)
 	}
 	return nil
 }
@@ -70,7 +71,7 @@ func (m *MemCmd) Run() error {
 // Start implements Start of Cmd
 func (m *MemCmd) Start() error {
 	if m.StartCallback != nil {
-		return m.StartCallback(m.BaseCmd)
+		return m.StartCallback(m)
 	}
 	return nil
 }
@@ -78,7 +79,7 @@ func (m *MemCmd) Start() error {
 // StderrPipe implements StderrPipe of Cmd
 func (m *MemCmd) StderrPipe() (io.ReadCloser, error) {
 	if m.StderrPipeCallback != nil {
-		return m.StderrPipeCallback(m.BaseCmd)
+		return m.StderrPipeCallback(m)
 	}
 	return nil, nil
 }
@@ -86,7 +87,7 @@ func (m *MemCmd) StderrPipe() (io.ReadCloser, error) {
 // StdinPipe implements StdinPipe of Cmd
 func (m *MemCmd) StdinPipe() (io.WriteCloser, error) {
 	if m.StdinPipeCallback != nil {
-		return m.StdinPipeCallback(m.BaseCmd)
+		return m.StdinPipeCallback(m)
 	}
 	return nil, nil
 }
@@ -94,7 +95,7 @@ func (m *MemCmd) StdinPipe() (io.WriteCloser, error) {
 // StdoutPipe implements StdoutPipe of Cmd
 func (m *MemCmd) StdoutPipe() (io.ReadCloser, error) {
 	if m.StdoutPipeCallback != nil {
-		return m.StdoutPipeCallback(m.BaseCmd)
+		return m.StdoutPipeCallback(m)
 	}
 	return nil, nil
 }
@@ -102,7 +103,7 @@ func (m *MemCmd) StdoutPipe() (io.ReadCloser, error) {
 // Wait implements Wait of Cmd
 func (m *MemCmd) Wait() error {
 	if m.WaitCallback != nil {
-		return m.WaitCallback(m.BaseCmd)
+		return m.WaitCallback(m)
 	}
 	return nil
 }
