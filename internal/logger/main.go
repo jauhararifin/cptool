0b12195e82e962438e48b4b43d797eacf2045f81
Jauhar Arifin <jauhararifin10@gmail.com>
2019-02-09 20:02:26 +0700

    refactor logger package, using type instead of global function, and add print, println, and printf method

--- internal/logger/main.go
+++ internal/logger/main.go
@@ -3,36 +3,89 @@ package logger
 import (
 	"fmt"
 	"io"
-	"os"
 )
 
-// OutputTarget specify where log should be written
-var OutputTarget io.Writer = os.Stderr
+// VERBOSE describe VERBOSE log level
+const VERBOSE uint = 2
+
+// DEBUG describe DEBUG log level
+const DEBUG uint = 3
+
+// INFO describe INFO log level
+const INFO uint = 4
+
+// WARN describe WARN log level
+const WARN uint = 5
+
+// ERROR describe ERROR log level
+const ERROR uint = 6
+
+// Logger handle logging activity to the output target
+type Logger struct {
+	OutputTarget io.Writer
+	LoggingLevel uint
+}
+
+// New creates instance of logger
+func New(outputTarget io.Writer, loggingLevel uint) *Logger {
+	return &Logger{
+		OutputTarget: outputTarget,
+		LoggingLevel: loggingLevel,
+	}
+}
+
+func (logger *Logger) printfLevel(level uint, format string, messages ...interface{}) {
+	if level >= logger.LoggingLevel {
+		fmt.Fprintf(logger.OutputTarget, format, messages...)
+	}
+}
+
+func (logger *Logger) printLevel(level uint, messages ...interface{}) {
+	if level >= logger.LoggingLevel {
+		fmt.Fprint(logger.OutputTarget, messages...)
+	}
+}
 
 // PrintInfo print message in screen with INFO level
-func PrintInfo(messages ...interface{}) {
-	fmt.Fprintf(OutputTarget, "\033[0;34m[   Info    ] ")
-	fmt.Fprint(OutputTarget, messages...)
-	fmt.Fprintf(OutputTarget, "\033[0m\n")
+func (logger *Logger) PrintInfo(messages ...interface{}) {
+	logger.printfLevel(INFO, "\033[0;34m[   Info    ] ")
+	logger.printLevel(INFO, messages...)
+	logger.printfLevel(INFO, "\033[0m\n")
 }
 
 // PrintWarning print message in screen with WARNING level
-func PrintWarning(messages ...interface{}) {
-	fmt.Fprint(OutputTarget, "\033[0;93m[   Warn    ] ")
-	fmt.Fprint(OutputTarget, messages...)
-	fmt.Fprintf(OutputTarget, "\033[0m\n")
+func (logger *Logger) PrintWarning(messages ...interface{}) {
+	logger.printLevel(WARN, "\033[0;93m[   Warn    ] ")
+	logger.printLevel(WARN, messages...)
+	logger.printfLevel(WARN, "\033[0m\n")
 }
 
 // PrintError print message in screen with ERROR level
-func PrintError(messages ...interface{}) {
-	fmt.Fprint(OutputTarget, "\033[0;31m[   Error   ] ")
-	fmt.Fprint(OutputTarget, messages...)
-	fmt.Fprintf(OutputTarget, "\033[0m\n")
+func (logger *Logger) PrintError(messages ...interface{}) {
+	logger.printLevel(ERROR, "\033[0;31m[   Error   ] ")
+	logger.printLevel(ERROR, messages...)
+	logger.printfLevel(ERROR, "\033[0m\n")
 }
 
 // PrintSuccess print message in screen with SUCCESS level
-func PrintSuccess(messages ...interface{}) {
-	fmt.Fprint(OutputTarget, "\033[0;32m[  Success  ] ")
-	fmt.Fprint(OutputTarget, messages...)
-	fmt.Fprintf(OutputTarget, "\033[0m\n")
+func (logger *Logger) PrintSuccess(messages ...interface{}) {
+	logger.printLevel(INFO, "\033[0;32m[  Success  ] ")
+	logger.printLevel(INFO, messages...)
+	logger.printfLevel(INFO, "\033[0m\n")
+}
+
+// Print print normal message in screen
+func (logger *Logger) Print(level uint, messages ...interface{}) {
+	logger.printLevel(level, messages...)
+}
+
+// Println print normal message in screen with endline
+func (logger *Logger) Println(level uint, messages ...interface{}) {
+	logger.printLevel(level, messages...)
+	logger.printLevel(level, "\n")
+}
+
+// Printf print normal message in screen with format
+func (logger *Logger) Printf(level uint, format string, messages ...interface{}) {
+	logger.printfLevel(level, format, messages...)
 }
--- internal/logger/main_test.go
+++ internal/logger/main_test.go
@@ -8,40 +8,140 @@ import (
 
 func TestPrintInfo(t *testing.T) {
 	buf := new(bytes.Buffer)
-	OutputTarget = buf
-	PrintInfo("just", "some", "test")
+	logger := New(buf, INFO)
+	logger.PrintInfo("just", "some", "test")
 	str := buf.String()
 	if !strings.Contains(str, "Info") || !strings.Contains(str, "justsometest") {
 		t.Fail()
 	}
 }
 
+func TestPrintInfoWithHigherLoggingLevel(t *testing.T) {
+	buf := new(bytes.Buffer)
+	logger := New(buf, INFO+1)
+	logger.PrintInfo("just", "some", "test")
+	str := buf.String()
+	if len(str) > 0 {
+		t.Fail()
+	}
+}
+
 func TestPrintWarning(t *testing.T) {
 	buf := new(bytes.Buffer)
-	OutputTarget = buf
-	PrintWarning("just", "some", "test")
+	logger := New(buf, WARN)
+	logger.PrintWarning("just", "some", "test")
 	str := buf.String()
 	if !strings.Contains(str, "Warn") || !strings.Contains(str, "justsometest") {
 		t.Fail()
 	}
 }
 
+func TestPrintWarningWithHigherLoggingLevel(t *testing.T) {
+	buf := new(bytes.Buffer)
+	logger := New(buf, WARN+1)
+	logger.PrintWarning("just", "some", "test")
+	str := buf.String()
+	if len(str) > 0 {
+		t.Fail()
+	}
+}
+
 func TestPrintError(t *testing.T) {
 	buf := new(bytes.Buffer)
-	OutputTarget = buf
-	PrintError("just", "some", "test")
+	logger := New(buf, ERROR)
+	logger.PrintError("just", "some", "test")
 	str := buf.String()
 	if !strings.Contains(str, "Error") || !strings.Contains(str, "justsometest") {
 		t.Fail()
 	}
 }
 
+func TestPrintErrorWithHigherLoggingLevel(t *testing.T) {
+	buf := new(bytes.Buffer)
+	logger := New(buf, ERROR+1)
+	logger.PrintError("just", "some", "test")
+	str := buf.String()
+	if len(str) > 0 {
+		t.Fail()
+	}
+}
+
 func TestPrintSuccess(t *testing.T) {
 	buf := new(bytes.Buffer)
-	OutputTarget = buf
-	PrintSuccess("just", "some", "test")
+	logger := New(buf, INFO)
+	logger.PrintSuccess("just", "some", "test")
 	str := buf.String()
 	if !strings.Contains(str, "Success") || !strings.Contains(str, "justsometest") {
+		t.Fail()
+	}
+}
+
+func TestPrintSuccessWithHigherLoggingLevel(t *testing.T) {
+	buf := new(bytes.Buffer)
+	logger := New(buf, INFO+1)
+	logger.PrintSuccess("just", "some", "test")
+	str := buf.String()
+	if len(str) > 0 {
+		t.Fail()
+	}
+}
+
+func TestPrint(t *testing.T) {
+	buf := new(bytes.Buffer)
+	logger := New(buf, 10)
+	logger.Print(11, "just", "some", "test")
+	str := buf.String()
+	if !strings.Contains(str, "justsometest") {
+		t.Fail()
+	}
+}
+
+func TestPrintWithHigherLevel(t *testing.T) {
+	buf := new(bytes.Buffer)
+	logger := New(buf, 10)
+	logger.Print(9, "just", "some", "test")
+	str := buf.String()
+	if len(str) > 0 {
+		t.Fail()
+	}
+}
+
+func TestPrintln(t *testing.T) {
+	buf := new(bytes.Buffer)
+	logger := New(buf, 10)
+	logger.Println(11, "just", "some", "test")
+	str := buf.String()
+	if !strings.Contains(str, "justsometest\n") {
+		t.Fail()
+	}
+}
+
+func TestPrintlnWithHigherLevel(t *testing.T) {
+	buf := new(bytes.Buffer)
+	logger := New(buf, 10)
+	logger.Println(9, "just", "some", "test")
+	str := buf.String()
+	if len(str) > 0 {
+		t.Fail()
+	}
+}
+
+func TestPrintf(t *testing.T) {
+	buf := new(bytes.Buffer)
+	logger := New(buf, 10)
+	logger.Printf(11, "%s-%s-%s", "just", "some", "test")
+	str := buf.String()
+	if !strings.Contains(str, "just-some-test") {
+		t.Fail()
+	}
+}
+
+func TestPrintfWithHigherLevel(t *testing.T) {
+	buf := new(bytes.Buffer)
+	logger := New(buf, 10)
+	logger.Printf(9, "%s-%s-%s", "just", "some", "test")
+	str := buf.String()
+	if len(str) > 0 {
 		t.Fail()
 	}
 }
